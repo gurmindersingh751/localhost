89dfd55efdc61f81ace14d77ca72cd23f0ff12a0
Gurminder singh <gurminder290195@gmail.com>
2018-07-31 17:20:49 -0400

    ch

--- RentACar/cars.php
+++ RentACar/cars.php
@@ -1,38 +1,39 @@
+<?php 
 
-<?php
 
-// Create connection
-$con=mysqli_connect("sql9.freemysqlhosting.net","sql9250012","AQQLGFKb74","sql9250012");
- 
-// Check connection
-if (mysqli_connect_errno())
-{
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+$id = isset($_REQUEST["id"]);
+$make = isset($_REQUEST["make"]);
+$model = isset($_REQUEST["model"]);
+$color = isset($_REQUEST["color"]);
+
+
+$file = parse_ini_file("https://gurmindersingh751.github.io/localhost/RentACar.ini");
+
+$host = trim($file["dbhost"]);
+$user = trim($file["dbuser"]);
+$pass = trim($file["dbpass"]);
+$name = trim($file["dbname"]);
+
+require("https://gurmindersingh751.github.io/localhost/RentACar/secure/access.php");
+$access = new access($host, $user, $pass,$name);
+$access->connect();
+
+$user = $access->selectCars();
+
+if(!empty($user)){
+    $returnArray["cars"] = $user;
+    echo json_encode($returnArray);
+    return;
 }
- 
-// This SQL statement selects ALL from the table 'Saved Parties'
-$sql = "SELECT * FROM cars";
- 
-// Check if there are results
-if ($result = mysqli_query($con, $sql))
-{
-	// If so, then create a results array and a temporary one
-	// to hold the data
-	$resultArray = array();
-	$tempArray = array();
- 
-	// Loop through each row in the result set
-	while($row = $result->fetch_object())
-	{
-		// Add each row into our results array
-		$tempArray = $row;
-	    array_push($resultArray, $tempArray);
-	}
- 
-	// Finally, encode the array to JSON and output the results
-	echo json_encode($resultArray);
-}
- 
-// Close connections
-mysqli_close($con);
+
+$returnArray["id"] = $user["id"];
+$returnArray["make"] = $user["make"];
+$returnArray["model"] = $user["model"];
+$returnArray["color"] = $user["color"];
+
+
+$access->disconnect();
+echo json_encode($returnArray);
+
+
 ?>
